ea89533c619b3a2936988277023d987027c7ac4f
serggl <sergey.glukhov@gmail.com>
2013-08-26 14:43:39 +0400

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-jquery-infinite-scroll
+jquery infinite scroll
 ======================
 
-JQuery plugin for ajax-enabled infinite page scroll
+JQuery plugin for ajax-enabled infinite page scroll.
+There are a lot of jQuery plugins for infinite page scrolling function.
+However none of them matched my needs when I faced the problem.
+So this is a very simple plugin which makes ajax call to load more data on the page and gives some basic callbacks like data loading started or data did loaded.
+
+
---
+++ src/jquery.infiniteScroll.js
@@ -0,0 +1,77 @@
+/**
+ * Your plugin name and a small description
+ * Author: Sergey Glukhov, AnjLab (http://anjlab.com)
+ * Version: 0.1
+ */
+
+(function($){
+
+    $.fn.infiniteScroll = function(settings){
+        var $this = $(this)
+        if (!$this.length) {
+            return $this;
+        }
+
+        var opts = $.extend({}, $.fn.infiniteScroll.defaults, settings);
+        var currentScrollPage = 1;
+        var scrollTriggered = 0;
+
+        $this.find(opts.itemSelector + ':last').addClass('last-scroll-row');
+
+        $(window).on('scroll', function() {
+            var row = $('.last-scroll-row');
+            if (row.length && !scrollTriggered && isScrolledIntoView(row)) {
+                scrollTriggered = 1;
+                triggerDataLoad();
+            }
+        });
+
+        function isScrolledIntoView(elem) {
+            var docViewTop = $(window).scrollTop();
+            var docViewBottom = docViewTop + $(window).height();
+            var elemTop = $(elem).offset().top;
+            var elemBottom = elemTop + $(elem).height();
+            return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+        }
+
+        function onDataLoaded(data) {
+            var prev = $('.last-scroll-row');
+            var content = $(data);
+            if (prev.length && content.length) {
+                content.insertAfter(prev);
+                prev.removeClass('last-scroll-row');
+                $this.find(opts.itemSelector + ':last').addClass('last-scroll-row');
+                scrollTriggered = 0;
+            }
+            if (jQuery.isFunction(opts.onDataLoaded)) {
+                opts.onDataLoaded(currentScrollPage);
+            }
+        }
+
+        function triggerDataLoad() {
+            currentScrollPage += 1;
+            if (jQuery.isFunction(opts.onDataLoading)) {
+                opts.onDataLoading(currentScrollPage);
+            }
+            $.get(opts.dataPath + '?page=' + currentScrollPage)
+                .always(onDataLoaded)
+                .fail(function() {
+                    if (jQuery.isFunction(opts.onDataError)) {
+                        opts.onDataError(currentScrollPage);
+                    }
+                });
+        }
+
+        return this;
+    }
+
+    // plugin defaults - added as a property on our plugin function
+    $.fn.infiniteScroll.defaults = {
+        dataPath: null,
+        itemSelector: '.item',
+        onDataLoading: null, // function (page)
+        onDataLoaded: null, // function (page)
+        onDataError: null // function (page)
+    }
+
+})(jQuery);
